aa7a66011bc1ba24528fb5c3b65f4730773fa7d8
Adharsraj <138186421+Adharsraj@users.noreply.github.com>
2024-06-21 18:21:59 +0530

    new

--- app/(root)/template/page.tsx
+++ app/(root)/template/page.tsx
@@ -7,6 +7,7 @@ import React from "react";
       <Link href="template/1">
         <div className="bg-red-600 p-5">template1</div>
       </Link>
+      
       <Link href="template/2">
         <div className="bg-yellow-600 p-5">template2</div>
       </Link>
